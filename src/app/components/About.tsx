342894933d889ccb00121827fa88def22a0d45fa
Wilfred Chen <wilfred@gmail.com>
2024-04-10 22:03:46 +1000

    Initial boilerplate; Install framer-motion package

---
+++ src/app/components/About.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <>
+            <div className="w-full h-[500px] ">
+                <div>
+                    <h1>Hey, I'm Wilfred. A front-end developer living in Melbourne, Australia. </h1>
+                </div>
+            </div>
+        </>
+    );
+}
