f469409327c6afa8149fcefaff46d6dd53429b60
Wilfred Chen <wilfred@gmail.com>
2024-05-29 21:20:26 +1000

    Add Lenis smooth scroll to main page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
 "use client";
 
 import Hero from "@/components/Hero";
+import Lenis from "lenis";
 
 export default function Home() {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+        console.log(e);
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
     return (
-        <main>
+        <main className="overflow-hidden">
+            <Hero />
             <Hero />
         </main>
     );
