9fa4f83b03dcba2f9202aae9448b8674b18a31f3
Wilfred Chen <wilfred@gmail.com>
2024-05-29 18:00:48 +1000

    Create preloader animation

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,25 +1,41 @@
 import { useRef } from "react";
 import styles from "./Hero.module.scss";
-import { nameIntro, wipeUpIntro } from "./animations";
+import { nameIntro, navIntro, socialsIntro, wipeUpIntro, preloaderAnimation } from "./animations";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 export default function Hero() {
-    const wipeUpRef = useRef(null);
+    const preloaderTextRef = useRef(null);
     const navRef = useRef(null);
     const nameRef = useRef(null);
     const socialsRef = useRef(null);
+    const preloaderRef = useRef(null);
+    const preloaderBackgroundRef = useRef(null);
 
     useGSAP(() => {
-        const tl = gsap.timeline();
+        const heroTimeline = gsap.timeline();
 
-        tl.add(nameIntro(nameRef)).add(wipeUpIntro(wipeUpRef), "<");
+        const setInitialStates = () => {
+            const selectSpans = gsap.utils.selector(preloaderTextRef);
+
+            gsap.set(selectSpans("span"), {
+                yPercent: 200,
+            });
+        };
+
+        setInitialStates();
+
+        heroTimeline
+            .add(setInitialStates())
+            .add(preloaderAnimation(preloaderTextRef, preloaderBackgroundRef))
+            .add(nameIntro(nameRef))
+            .add(navIntro(navRef), "-=0.5")
+            .add(socialsIntro(socialsRef), "<");
     }, []);
+
     return (
         <>
             <div className={styles.hero}>
-                <div className={styles.hero__wipeUp} ref={wipeUpRef}></div>
-
                 <div className={styles.hero__nav} ref={navRef}>
                     <ul>
                         <li>
@@ -34,9 +50,18 @@ export default function Hero() {
                     </ul>
                 </div>
 
+                <div className={styles.preloader} ref={preloaderRef}>
+                    <p className={styles.preloader__text} ref={preloaderTextRef}>
+                        <span className={styles.preloader__mainText}>Wilfred Chen</span>
+                        <span className={styles.preloader__subText}>Portfolio 2024</span>
+                    </p>
+
+                    <div className={styles.preloader__background} ref={preloaderBackgroundRef}></div>
+                </div>
+
                 <div className={styles.hero__name} ref={nameRef}>
                     <h1>Wilfred Chen</h1>
-                    <h2>Portfolio</h2>
+                    <h2>Web Developer</h2>
                 </div>
 
                 <div className={styles.hero__socials} ref={socialsRef}>
