4d3a8f51b32ddcd14c593f7330919036238ec88f
Wilfred Chen <wilfred@gmail.com>
2024-05-29 18:15:26 +1000

    Delete unused code in main page.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,13 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import Loader from "@/components/Loader";
-import HelloWorld from "@/components/HelloWorld";
-import Navbar from "@/components/Navbar";
-import Projects from "@/components/Projects";
 import { useLayoutEffect, useState } from "react";
 import gsap from "gsap";
 
 export default function Home() {
-    const [loaderFinished, setLoaderFinished] = useState(false);
-    const [timeline, setTimeline] = useState(null);
-
-    useLayoutEffect(() => {
-        const context = gsap.context(() => {
-            const tl = gsap.timeline({
-                onComplete: () => setLoaderFinished(true),
-            });
-
-            setTimeline(tl);
-        });
-
-        return () => context.revert();
-    }, []);
-
     return (
         <main>
-            {/* {loaderFinished ? <Hero /> : <Loader timeline={timeline} />} */}
-
             <Hero />
-
-            {/* <Loader timeline={timeline} /> */}
         </main>
     );
 }
