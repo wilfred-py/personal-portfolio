3246e9a64a0e675ceed421fd49bbe5357f739da4
Wilfred Chen <wilfred@gmail.com>
2024-05-29 18:19:04 +1000

    Remove unused code in app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import { useLayoutEffect, useState } from "react";
-import gsap from "gsap";
 
 export default function Home() {
     return (
