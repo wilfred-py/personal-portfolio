c2dcc1be1cff9c4a662f22e28bf75d891dff3f7a
Wilfred Chen <wilfred@gmail.com>
2024-06-01 18:20:02 +1000

    Add background image

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,6 +6,8 @@ import gsap from "gsap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import dolomitesImage from "@/public/images/dolomites-background.jpg";
 
 export default function Hero() {
     const preloaderTextRef = useRef(null);
@@ -39,6 +41,13 @@ export default function Hero() {
     return (
         <>
             <div className={styles.hero}>
+                <Image
+                    src={dolomitesImage}
+                    alt="Picture of mountain ranges in Dolomites, Italy"
+                    className={styles.hero__backgroundImage}
+                    priority={true}
+                />
+
                 <div className={styles.hero__nav} ref={navRef}>
                     <ul>
                         <li>
@@ -72,21 +81,6 @@ export default function Hero() {
                         <li>
                             <a href="https://github.com/wilfred-py" target="_blank">
                                 <FontAwesomeIcon icon={faGithub} bounce size="xl" />
-                                {/* <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width={24}
-                                    height={24}
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    strokeWidth={2}
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
-                                >
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-                                </svg> */}
                             </a>
                         </li>
                         <li>
